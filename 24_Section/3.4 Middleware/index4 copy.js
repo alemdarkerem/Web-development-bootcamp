f05f1cfdaccf8fc3d18080a2bc454fa002e5cd16
alemdarkerem <alemdarkerem1907@gmail.com>
2024-01-04 17:31:43 +0300

    custom middlewares completed.

--- 24_Section/3.4 Middleware/index4 copy.js
+++ 24_Section/3.4 Middleware/index4 copy.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import {dirname} from "path";
-import {fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
@@ -19,12 +19,13 @@ function bandNameGenerator (req, res, next) {
 app.use(bandNameGenerator);
 
 app.post("/submit", (req, res) => {
-  res.send(`<h1>Your Band Name is:</h1><h2>${bandName}</h2>`);
-})
+  res.send(`<h1>Your band name is:</h1><h2>${bandName}</h2>`);
+});
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
-})
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
